e581985eefc1b42bf298b1c46c53ee098ef67722
ancientgoat <37283728Sonom--+>
2016-03-25 09:12:08 -0600

    Check in

--- service/src/main/java/com/premierinc/service/ChildEntityService.java
+++ service/src/main/java/com/premierinc/service/ChildEntityService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 /**
